cea2038eebe9e2ee5f4f32afee91118bdcb2bd09
John Hawley <madsci@google.com>
2016-01-13 18:18:49 -0800

    Support all but multidim measurements

--- openhtf/util/validators.py
+++ openhtf/util/validators.py
@@ -81,55 +81,55 @@ class Validators(object):
         raise ValueError('Must specify minimum, maximum, or both')
       if minimum is not None and maximum is not None and minimum > maximum:
         raise ValueError('Minimum cannot be greater than maximum')
-      self._minimum = minimum
-      self._maximum = maximum
+      self.minimum = minimum
+      self.maximum = maximum
   
     def __call__(self, value):
       # Check for equal bounds first so we can use with non-numeric values.
-      if self._minimum == self._maximum and value != self._minimum:
+      if self.minimum == self.maximum and value != self.minimum:
         return False
-      if self._minimum is not None and value < self._minimum:
+      if self.minimum is not None and value < self.minimum:
         return False
-      if self._maximum is not None and value > self._maximum:
+      if self.maximum is not None and value > self.maximum:
         return False
       return True
   
     def __str__(self):
-      assert self._minimum is not None or self._maximum is not None
-      if self._minimum is not None and self._maximum is not None:
-        if self._minimum == self._maximum:
-          return 'x == %s' % self._minimum
-        return '%s <= x <= %s' % (self._minimum, self._maximum)
-      if self._minimum is not None:
-        return '%s <= x' % self._minimum
-      if self._maximum is not None:
-        return 'x <= %s' % self._maximum
+      assert self.minimum is not None or self.maximum is not None
+      if self.minimum is not None and self.maximum is not None:
+        if self.minimum == self.maximum:
+          return 'x == %s' % self.minimum
+        return '%s <= x <= %s' % (self.minimum, self.maximum)
+      if self.minimum is not None:
+        return '%s <= x' % self.minimum
+      if self.maximum is not None:
+        return 'x <= %s' % self.maximum
   
   @classmethod
   def Equals(cls, value):
     return cls.InRange(minimum=value, maximum=value)
       
-  class _MatchesRegex(object):
+  class RegexMatcher(object):
     """Validator to verify a string value matches a regex."""
   
     def __init__(self, re_module, regex):
       self._re_module = re_module
       self._compiled = re_module.compile(regex)
-      self._regex = regex
+      self.regex = regex
   
     def __call__(self, value):
       return self._compiled.match(str(value)) is not None
 
     def __deepcopy__(self, dummy_memo):
-      return type(self)(self._re_module, self._regex)
+      return type(self)(self._re_module, self.regex)
   
     def __str__(self):
-      return "'x' matches /%s/" % self._regex
+      return "'x' matches /%s/" % self.regex
 
   # We have to use our saved reference to the re module because this module
   # has lost all references by the sys.modules replacement and has been gc'd.
   def MatchesRegex(self, regex):
-    return self._MatchesRegex(self.re_module, regex)
+    return self.RegexMatcher(self.re_module, regex)
 
 
 sys.modules[__name__] = Validators(re)
