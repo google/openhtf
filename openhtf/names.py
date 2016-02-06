f1ce939cfa6c3d918523c5520bf4ef35871e0ded
John Hawley <madsci@google.com>
2016-02-05 18:08:23 -0800

    refer directly to output.json_factory

--- openhtf/names.py
+++ openhtf/names.py
@@ -31,7 +31,7 @@ Unit codes for lookup:
 """
 
 import openhtf
-import openhtf.io.output
+import openhtf.io.output.json_factory
 import openhtf.io.user_input
 import openhtf.plugs
 import openhtf.util.measurements
@@ -54,6 +54,6 @@ plug = openhtf.plugs.requires
 
 # Classes used in writing test scripts.
 Measurement = openhtf.util.measurements.Measurement
-OutputToJSON = openhtf.io.output.OutputToJSON
+OutputToJSON = openhtf.io.output.json_factory.OutputToJSON
 TestPhase = openhtf.TestPhaseOptions
 UOM = openhtf.util.units.UOM
