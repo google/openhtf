8297f421a793d4a537e77529d608da280155a38d
Fahrzin Hemmati <fahhem@gmail.com>
2016-03-23 14:43:44 -0700

    Change AutoStart to take a serial argument

--- openhtf/exe/triggers.py
+++ openhtf/exe/triggers.py
@@ -38,20 +38,13 @@ TestStop:
 import logging
 import time
 
-import gflags
-
 from openhtf.io import user_input
 
-gflags.DEFINE_string('dut_serial', 'UNKNOWN_DUT_ID',
-                     'DUT serial to start the test with. '
-                     'Only use if using the AutoStart trigger.')
-
-FLAGS = gflags.FLAGS
 _LOG = logging.getLogger(__name__)
 
-def AutoStart():  # pylint: disable=invalid-name
-  """Start the test immediately with a dummy DUT ID."""
-  return FLAGS.dut_serial
+def AutoStart(serial='UNKNOWN_DUT_ID'):  # pylint: disable=invalid-name
+  """Start the test immediately with the given serial (or a dummy DUT ID)."""
+  return serial
 
 
 def AutoStop(dummy_dut_id):  # pylint: disable=invalid-name
@@ -59,7 +52,6 @@ def AutoStop(dummy_dut_id):  # pylint: disable=invalid-name
   pass
 
 
-# pylint: disable=invalid-name
 def PromptForTestStart(message='Provide a DUT ID in order to start the test.',
                        text_input=True):
   """Make a test start trigger based on prompting the user for input."""
