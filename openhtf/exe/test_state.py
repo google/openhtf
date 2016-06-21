8ea9e58a01fe917c9675207d85695725f77fff43
John Hawley <madsci@skybox.com>
2016-06-21 03:25:51 -0700

    shuffle things around a little, decouple test state in a few ways, giving it a more concise purpose

--- openhtf/exe/test_state.py
+++ openhtf/exe/test_state.py
@@ -17,6 +17,9 @@
 
 Test timing, failures, and the UI are handled by this module.
 """
+
+import copy
+import cPickle as pickle
 import json
 import logging
 
@@ -24,6 +27,7 @@ from enum import Enum
 
 import openhtf
 
+from openhtf import conf
 from openhtf import util
 from openhtf.exe import phase_data
 from openhtf.io import test_record
@@ -34,58 +38,50 @@ from openhtf.util import measurements
 _LOG = logging.getLogger(__name__)
 
 
-class BlankDutIdError(Exception):
-  """DUT serial cannot be blank at the end of a test."""
-
-
-class FrameworkError(Exception):
-  """Raised when we end up in an invalid internal state somehow."""
-
-
-class TestRecordAlreadyFinishedError(Exception):
-  """Raised when trying to finalize a test record that is already finished."""
-
-
 class TestState(object):
   """This class handles tracking the state of a running Test.
 
-  Args:
-    test_data: openhtf.TestData instance describing the test to run.
-    plug_manager: A plugs.PlugManager instance.
+  This class encapsulates all the interesting transient bits of a running Test,
+  as opposed to the openhtf.TestData class, which encapsulates static data
+  associated with a Test (that is, it remains the same across invokations of
+  Test.Execute()).
+
+  Init Args:
+    test_data: openhtf.TestData instance describing the test to run, used to
+        initialize some values here, no internal reference directly to it is
+        kept, and it is not modified.
+    plug_manager: A plugs.PlugManager instance.  Plug state is completely
+        torn down and reset upon each call to Test.Execute(), so plug state
+        is considered transient.
     dut_id: DUT identifier, if it's known, otherwise None.
-    station_id: Station identifier.
   """
   State = Enum('State', ['CREATED', 'RUNNING', 'COMPLETED'])
 
-  def __init__(self, test_data, plug_manager, dut_id, station_id):
+  def __init__(self, test_data, plug_manager, dut_id):
     self._state = self.State.CREATED
-    self.record = test_record.TestRecord(
-        dut_id=dut_id, station_id=station_id, code_info=test_data.code_info,
-        metadata=test_data.metadata)
+    self.test_record = test_record.TestRecord(
+        dut_id=dut_id, station_id=conf.station_id,
+        code_info=test_data.code_info,
+        # Copy metadata so we don't modify test_data.
+        metadata=copy.deepcopy(test_data.metadata))
     # TODO(madsci): Make RECORD_LOGGER work with multiple Test instances.
     self.logger = logging.getLogger(logs.RECORD_LOGGER)
-    self._record_handler = logs.RecordHandler(self.record)
+    self._record_handler = logs.RecordHandler(self.test_record)
     self.logger.addHandler(self._record_handler)
-    self.phase_data = phase_data.PhaseData(self.logger, plug_manager, self.record)
+    self.phase_data = phase_data.PhaseData(
+        self.logger, plug_manager, self.test_record)
     self.running_phase_record = None
-    self.pending_phases = list(test_data.phases)
 
   def __del__(self):
     self.logger.removeHandler(self._record_handler)
-
-  def AsJSON(self):
-    """Return JSON representation of the test's serialized state."""
-    return json.dumps(data.ConvertToBaseTypes(self))
 
   def _asdict(self):
     """Return a dict representation of the test's state."""
     return {
         'status': self._state.name,
-        'record': self.record,
-        'phase_data': self.phase_data,
-        'running_phase_record': self.running_phase_record,
-        'pending_phases': [(phase.func.__name__, phase.func.__doc__)
-                           for phase in self.pending_phases]}
+        'test_record': pickle.dumps(self.test_record, -1),
+        'running_phase_record': pickle.dumps(self.running_phase_record, -1),
+    }
 
   def GetLastRunPhaseName(self):
     """Get the name of the currently running phase, or None.
@@ -110,17 +106,17 @@ class TestState(object):
     """
     # Handle a few cases where the test is ending prematurely.
     if phase_outcome.raised_exception:
-      self.record.outcome = test_record.Outcome.ERROR
+      self.test_record.outcome = test_record.Outcome.ERROR
       code = str(type(phase_outcome.phase_result).__name__)
       description = str(phase_outcome.phase_result).decode('utf8', 'replace')
-      self.record.AddOutcomeDetails(code, description)
+      self.test_record.AddOutcomeDetails(code, description)
       self._state = self.State.COMPLETED
     elif phase_outcome.is_timeout:
-      self.record.outcome = test_record.Outcome.TIMEOUT
+      self.test_record.outcome = test_record.Outcome.TIMEOUT
       self._state = self.State.COMPLETED
     elif phase_outcome.phase_result == openhtf.PhaseResult.STOP:
       # TODO(madsci): Decouple flow control from pass/fail.
-      self.record.outcome = test_record.Outcome.FAIL
+      self.test_record.outcome = test_record.Outcome.FAIL
       self._state = self.State.COMPLETED
 
     return self._state == self.State.COMPLETED
@@ -132,44 +128,16 @@ class TestState(object):
   def SetStateFinished(self):
     """Mark the state as finished, only called if the test ended normally."""
     if any(meas.outcome != measurements.Outcome.PASS
-           for phase in self.record.phases
+           for phase in self.test_record.phases
            for meas in phase.measurements.itervalues()):
-      self.record.outcome = test_record.Outcome.FAIL
+      self.test_record.outcome = test_record.Outcome.FAIL
     else:
-      self.record.outcome = test_record.Outcome.PASS
-
-  def GetFinishedRecord(self):
-    """Get a test_record.TestRecord for the finished test.
-
-    Should only be called once at the conclusion of a test run, and will raise
-    an exception if end_time_millis is already set.
-
-    Returns:  An updated test_record.TestRecord that is ready for output.
-
-    Raises: TestRecordAlreadyFinishedError if end_time_millis already set.
-    """
-    if self.record.end_time_millis:
-      raise TestRecordAlreadyFinishedError('Test already finished at',
-                                           self.record.end_time_millis)
-
-    if not self.record.dut_id:
-      raise BlankDutIdError(
-          'Blank or missing DUT ID, HTF requires a non-blank ID.')
-
-    if not self.record.outcome:
-      raise FrameworkError(
-          'Internal framework error, test outcome unset!')
-
+      self.test_record.outcome = test_record.Outcome.PASS
     self.logger.debug('Finishing test execution with outcome %s.',
-                      self.record.outcome.name)
-
-    self.record.end_time_millis = util.TimeMillis()
-    self.logger.removeHandler(self._record_handler)
-    return self.record
+                      self.test_record.outcome.name)
 
   def __str__(self):
     return '<%s: %s@%s Running Phase: %s>' % (
-        type(self).__name__, self.record.dut_id, self.record.station_id,
-        self.GetLastRunPhaseName(),
+        type(self).__name__, self.test_record.dut_id,
+        self.test_record.station_id, self.GetLastRunPhaseName(),
     )
-  __repr__ = __str__
