8a8531fc96b5fc884ba2b8a083ee522e95025c1f
madsci <madsci@google.com>
2016-07-04 22:50:30 -0700

    clean up serverproxy mixins

--- openhtf/util/xmlrpcutil.py
+++ openhtf/util/xmlrpcutil.py
@@ -16,48 +16,61 @@
 
 import functools
 import httplib
+import threading
 import xmlrpclib
 
 
 class TimeoutHTTPConnection(httplib.HTTPConnection):
+  def __init__(self, timeout_s, *args, **kwargs):
+    httplib.HTTPConnection.__init__(self, *args, **kwargs)
+    self.timeout_s = timeout_s
+
   def connect(self):
     httplib.HTTPConnection.connect(self)
     self.sock.settimeout(self.timeout_s)
-
-
-class TimeoutHTTP(httplib.HTTP):
-  _connection_class = TimeoutHTTPConnection
-
-  def set_timeout(self, timeout_s):
-    self._conn.timeout_s = timeout_s
 
 
 class TimeoutTransport(xmlrpclib.Transport):
 
   def __init__(self, timeout_s, *args, **kwargs):
     xmlrpclib.Transport.__init__(self, *args, **kwargs)
+    self._connection = None
     self.timeout_s = timeout_s
 
   def make_connection(self, host):
-    connection = TimeoutHTTP(host)
-    connection.set_timeout(self.timeout_s)
-    return conn
+    if self._connection and host == self._connection[0]:
+      return self._connection[1]
+    self._connection = host, TimeoutHTTPConnection(self.timeout_s, host)
+    return self._connection[1]
 
 
-class LockedTimeoutServerProxy(xmlrpclib.ServerProxy):
-  """A ServerProxy that locks RPC calls and supports timeouts."""
-  def __init__(self, host, port, timeout_s=5, *args, **kwargs):
-    xmlrpclib.ServerProxy.__init__(
-        self, 'http://%s:%s' % (host, port),
-        transport=TimeoutTransport(timeout_s), *args, **kwargs)
+class BaseServerProxy(xmlrpclib.ServerProxy, object):
+  """New-style base class for ServerProxy, allows for use of Mixins below."""
+
+
+class TimeoutServerProxyMixin(object):
+  """A ServerProxy that supports timeouts."""
+  def __init__(self, *args, **kwargs):
+    super(TimeoutServerProxyMixin, self).__init__(
+        transport=TimeoutTransport(kwargs.pop('timeout_s', 5)),
+        *args, **kwargs)
+
+
+class LockedServerProxyMixin(object):
+  """A ServerProxy that locks calls to methods."""
+  def __init__(self, *args, **kwargs):
+    super(LockedServerProxyMixin, self).__init__(*args, **kwargs)
     self._lock = threading.Lock()
 
   def __getattr__(self, attr):
-    attr = xmlrpclib.ServerProxy.__getattr__(self, attr)
-    if callable(attr):
-      @functools.wraps(attr)
-      def _Wrapper(*args, **kwargs):
+    method = super(LockedServerProxyMixin, self).__getattr__(attr)
+    if callable(method):
+      # xmlrpc doesn't support **kwargs, so only accept *args.
+      def _Wrapper(*args):
         with self._lock:
-          return attr(*args, **kwargs)
+          return method(*args)
+      # functools.wraps() doesn't work with _Method internal type within
+      # xmlrpclib.  We only care about the name anyway, so manually set it.
+      _Wrapper.__name__ = attr
       return _Wrapper
-    return attr
+    return method
