aeec4c61e8a1e4f4481684c51c7b27e549d86ba9
madsci <madsci@google.com>
2016-07-07 15:09:47 -0700

    be more tolerant of pytest version updates

--- setup.py
+++ setup.py
@@ -182,7 +182,7 @@ setup(
     ],
     tests_require=[
         'mock>=2.0.0',
-        'pytest==2.8.7',
-        'pytest-cov==2.2.1',
+        'pytest>=2.9.2',
+        'pytest-cov>=2.2.1',
     ],
 )
