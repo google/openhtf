ddeb1308a5962627266471b37b200096eca72eda
Madeline Chan <maddychan@maddychan0.mtv.corp.google.com>
2016-07-14 16:22:30 -0700

    Got conf plugin to work, though doesn't check to make sure dynamically instantiated attributes are defined in the same module.

--- pylint_plugins/openhtf_pylint.py
+++ pylint_plugins/openhtf_pylint.py
@@ -4,7 +4,11 @@ import astroid
 from astroid import MANAGER
 from pylint import lint
 
+def __init__(self):
+  pass
 
+
+#didn't change/work on this yet
 def wrap_add_message(func):
   def add_message(msg_descr, line=None, node=None, args=None, confidence=None):
     kwargs = {'confidence': confidence} if confidence else {}
@@ -24,33 +28,52 @@ def wrap_add_message(func):
 
   return add_message
 
+#storing a reference to the openhtf.conf module node
+clss = []
 
 def trans_expr(node):
   if (isinstance(node.func, astroid.Attribute) and
       isinstance(node.func.expr, astroid.Name) and
       node.func.expr.name == 'conf' and
       node.func.attrname == 'Declare'):
-    import pdb;pdb.set_trace()
-    #print('hello?\n\n\n')
-    # We're declaring a conf key, stash it in the root (astroid.Module).
-    if node.args:
-      conf_key_name = node.args[0].value
-    else:
-      for keyword in node.keywords:
-        if keyword.arg == 'name':
-          # We assume the name is an astroid.Const(str), so it has a str value.
-          conf_key_name = keyword.value.value
-          break
-      else:
-        # TODO(maddychan): Figure out how to cleanly report a pylint error,
-        # here the error is invalid conf.Declare() syntax (no name given).
-        pass
-    try:
-      node.root()._declared_conf_keys.append(conf_key_name)
-    except AttributeError:
-      node.root()._declared_conf_keys = [conf_key_name]
+
+
+    if node.args and clss:
+      clss[0].locals[node.args[0].value] = [None]
+  
+
+    # madsci's stuff - didn't use it sorry.... 
+    #  
+    # else:
+    #   for keyword in node.keywords:
+    #     if keyword.arg == 'name':
+    #       # We assume the name is an astroid.Const(str), so it has a str value.
+    #       conf_key_name = keyword.value.value
+    #       break
+    #   else:
+    #     # TODO(maddychan): Figure out how to cleanly report a pylint error,
+    #     # here the error is invalid conf.Declare() syntax (no name given).
+    #     pass
+    # #try:
+    # #  node.root()._declared_conf_keys.append(conf_key_name)
+    # #except AttributeError:
+    # #  node.root()._declared_conf_keys = [conf_key_name]
+
+
+
+
+def conf_transform(cls):
+  if cls.name == 'openhtf.conf':
+    #putting all the attributes in Configuration into the openhtf.conf node
+    cls._locals.update(cls.locals['Configuration'][0].locals)
+    #cls._locals.update(members) DOESN'T WORK, this method is only called once in the beginning
+
+    #storing reference to this node for future use
+    clss.append(cls)
+
 
 
 def register(linter):
   MANAGER.register_transform(astroid.Call, trans_expr)
+  MANAGER.register_transform(astroid.Module, conf_transform)
   linter.add_message = wrap_add_message(linter.add_message)
