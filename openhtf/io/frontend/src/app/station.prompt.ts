d8f95b03c56a439d9272b74cf6644249112990d2
Madeline Chan <maddychan@google.com>
2016-08-26 10:49:11 -0700

    Renamed files
    Attempt at working with prompt

--- openhtf/io/frontend/src/app/station.prompt.ts
+++ openhtf/io/frontend/src/app/station.prompt.ts
@@ -19,7 +19,7 @@ import {Component, Input} from 'angular2/core';
 import {RouteParams, Router} from 'angular2/router';
 import {Observable} from 'rxjs/Observable';
 
-import {StationService} from './station.service';
+import {PlugService} from './plug.service';
 import 'file?name=/styles/station.prompt.css!./station.prompt.css';
 import 'file?name=/templates/station.prompt.html!./station.prompt.html';
 
@@ -30,26 +30,35 @@ import 'file?name=/templates/station.prompt.html!./station.prompt.html';
   templateUrl: 'templates/station.prompt.html',
   styleUrls: ['styles/station.prompt.css'],
   directives: [],
-  providers: [StationService]
+  providers: [PlugService]
 })
 export class Prompt {
   @Input() framework: any;
+  private states: any;
 
   /**
    * Create a Station view component.
    */
   constructor(private routeParams: RouteParams,
-              private stationService: StationService) {
+              private plugService: PlugService) {
+    
   }
 
   /**
    * Send the response to the given prompt through the StationService.
    */
   sendResponse(input: HTMLInputElement, id) {
+<<<<<<< HEAD:openhtf/output/web_gui/src/app/station.prompt.ts
     let ip = this.routeParams.get('ip');
     let port = this.routeParams.get('port');
     let response = input ? input.value : '';
     this.stationService.respondToPrompt(ip, port, id, response);
+=======
+    let response = input.value || '';
+    this.plugService.respondToPrompt(response);
+>>>>>>> c35267c... Renamed files:openhtf/io/frontend/src/app/station.prompt.ts
     input.value = null;
   }
+  //ngoninit to init the service?? 
+  //when to look at the state? do I just put getState() in the html? 
 }
