8a48c31137536fb4f6422d6cc43efd58a263639b
Joe Ethier <jethier@google.com>
2016-09-21 13:50:12 -0700

    Update version number for v1.0.3 release.

--- setup.py
+++ setup.py
@@ -158,7 +158,7 @@ class PyTestCommand(test):
 
 setup(
     name='openhtf',
-    version='1.0.2',
+    version='1.0.3',
     description='OpenHTF, the open hardware testing framework.',
     author='John Hawley',
     author_email='madsci@google.com',
