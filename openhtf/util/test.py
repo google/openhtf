c058e1ff9e401895c3ae37ca479dc8901eb90739
Fahrzin Hemmati <fahhem@users.noreply.github.com>
2017-05-26 17:10:35 -0700

    Format the error in util.test correctly (#585)

--- openhtf/util/test.py
+++ openhtf/util/test.py
@@ -314,12 +314,13 @@ def patch_plugs(**mock_plugs):
 
 
 class TestCase(unittest.TestCase):
-  
+
   def __init__(self, methodName=None):
     super(TestCase, self).__init__(methodName=methodName)
     test_method = getattr(self, methodName)
     if inspect.isgeneratorfunction(test_method):
-      raise ValueError("%s yields without @openhtf.util.test.yields_phases")
+      raise ValueError(
+          "%s yields without @openhtf.util.test.yields_phases" % methodName)
 
   def _AssertPhaseOrTestRecord(func):  # pylint: disable=no-self-argument,invalid-name
     """Decorator for automatically invoking self.assertTestPhases when needed.
