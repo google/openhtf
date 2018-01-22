ca508b1d28949350ec5dd26e4ba2789638265819
Joe Ethier <jethier@google.com>
2018-01-22 14:44:32 -0800

    Revert version bump for splitting into a separate PR.

--- setup.py
+++ setup.py
@@ -178,7 +178,7 @@ class PyTestCommand(test):
 
 setup(
     name='openhtf',
-    version='1.2.2',
+    version='1.2.1',
     description='OpenHTF, the open hardware testing framework.',
     author='John Hawley',
     author_email='madsci@google.com',
