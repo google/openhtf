32a53815868ab5519f1995861dfef7525f3d2b07
Kenneth Schiller <Kenadia@users.noreply.github.com>
2018-02-22 17:28:45 -0800

    Fix 'cannot marshall PlugDescriptor' in web_gui (#721)
    * Fix 'cannot marshall PlugDescriptor' in web_gui
    
    * Fix plugs test

--- test/plugs_test.py
+++ test/plugs_test.py
@@ -88,7 +88,7 @@ class PlugsTest(test.TestCase):
         self.plug_manager.provide_plugs(
             (('adder_plug', AdderPlug),))['adder_plug'])
     self.assertEqual(self.plug_manager._asdict()['plug_descriptors'], {
-            'plugs_test.AdderPlug': plugs.PlugDescriptor(['plugs_test.AdderPlug']),
+            'plugs_test.AdderPlug': {'mro': ['plugs_test.AdderPlug']},
         })
     self.assertEqual(self.plug_manager._asdict()['plug_states'], {
             'plugs_test.AdderPlug': {'number': 0},
