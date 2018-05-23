2fa34076fa6f9dcbd60410da1548e3170b4a778a
arsharma <arsharma@google.com>
2018-05-23 15:52:06 -0700

    Implement PhaseGroups.
    Implement PhaseGroups, which allow for setup and teardown contexts.  Teardown
    phases in such groups are guaranteed to run if the setup phases are all
    non-terminal.
    
    See docs/event_sequence.md for more information.

--- openhtf/core/test_executor.py
+++ openhtf/core/test_executor.py
@@ -22,6 +22,7 @@ import contextlib2 as contextlib
 
 import openhtf
 from openhtf.core import phase_executor
+from openhtf.core import phase_group
 from openhtf.core import test_state
 from openhtf.util import conf
 from openhtf.util import threads
@@ -29,8 +30,6 @@ from openhtf.util import threads
 
 _LOG = logging.getLogger(__name__)
 
-conf.declare('teardown_timeout_s', default_value=30, description=
-             'Timeout (in seconds) for test teardown functions.')
 conf.declare('cancel_timeout_s', default_value=2,
              description='Timeout (in seconds) when the test has been cancelled'
              'to wait for the running phase to exit.')
@@ -57,22 +56,26 @@ class TestExecutor(threads.KillableThread):
     self._test_start = test_start
     self._test_options = test_options
     self._lock = threading.Lock()
-    self._exit_stack = None
+    self._phase_exec = None
     self.uid = execution_uid
-    self._latest_outcome = None
+    self._last_outcome = None
+    self._abort = threading.Event()
+    self._full_abort = threading.Event()
+    self._teardown_phases_lock = threading.Lock()
 
-  def stop(self):
-    """Stop this test."""
-    _LOG.debug('Stopping test executor.')
+  def abort(self):
+    """Abort this test."""
+    if self._abort.is_set():
+      _LOG.error('Abort already set; forcibly stopping the process.')
+      self._full_abort.set()
+      self._stop_phase_executor(force=True)
+      return
+    _LOG.error('Abort test executor.')
     # Deterministically mark the test as aborted.
-    self.finalize()
-    # Cause the exit stack to collapse immediately.
-    with self._lock:
-      if self._exit_stack:
-        self._exit_stack.close()
-    # No need to kill this thread because, with the test_state finalized, it
-    # will end soon since we are stopping the current phase and won't run
-    # anymore phases.
+    self._abort.set()
+    self._stop_phase_executor()
+    # No need to kill this thread because the abort state has been set, it will
+    # end as soon as all queued teardown phases are run.
 
   def finalize(self):
     """Finalize test execution and output resulting record to callbacks.
@@ -92,28 +95,22 @@ class TestExecutor(threads.KillableThread):
     if self.test_state.test_record.dut_id is None:
       _LOG.warning('DUT ID is still not set; using default.')
       self.test_state.test_record.dut_id = self._test_options.default_dut_id
-    if not self.test_state.is_finalized:
-      self.test_state.logger.debug('Finishing test with outcome ABORTED.')
-      self.test_state.abort()
 
     return self.test_state
 
   def wait(self):
     """Waits until death."""
-    try:
-      # Timeout needed for SIGINT handling.
-      if sys.version_info >= (3, 2):
-        self.join(threading.TIMEOUT_MAX)
-      else:
-        # Seconds in a year.
-        self.join(31557600)
-    except KeyboardInterrupt:
-      self.test_state.logger.debug('KeyboardInterrupt caught, aborting test.')
-      raise
+    # Must use a timeout here in case this is called from the main thread.
+    # Otherwise, the SIGINT abort logic in test_descriptor will not get called.
+    if sys.version_info >= (3, 2):
+      self.join(threading.TIMEOUT_MAX)
+    else:
+      # Seconds in a year.
+      self.join(31557600)
 
   def _thread_proc(self):
     """Handles one whole test from start to finish."""
-    with contextlib.ExitStack() as exit_stack:
+    try:
       # Top level steps required to run a single iteration of the Test.
       self.test_state = test_state.TestState(
           self._test_descriptor,
@@ -123,20 +120,11 @@ class TestExecutor(threads.KillableThread):
 
       # Any access to self._exit_stacks must be done while holding this lock.
       with self._lock:
-        self._exit_stack = exit_stack
-        # Ensure that we tear everything down when exiting.
-        exit_stack.callback(self._execute_test_teardown, phase_exec)
-        # We don't want to run the 'teardown function' unless the test has
-        # actually started.
-        self._do_teardown_function = False
+        self._phase_exec = phase_exec
 
-      if self._test_start is not None and self._execute_test_start(phase_exec):
+      if self._test_start is not None and self._execute_test_start():
         # Exit early if test_start returned a terminal outcome of any kind.
         return
-      # The trigger has run and the test has started, so from now on we want the
-      # teardown function to execute at the end, no matter what.
-      with self._lock:
-        self._do_teardown_function = True
       self.test_state.mark_test_started()
 
       # Full plug initialization happens _after_ the start trigger, as close to
@@ -146,7 +134,10 @@ class TestExecutor(threads.KillableThread):
         return
 
       # Everything is set, set status and begin test execution.
-      self._execute_test_phases(phase_exec)
+      self.test_state.set_status_running()
+      self._execute_phase_group(self._test_descriptor.phase_group)
+    finally:
+      self._execute_test_teardown()
 
   def _initialize_plugs(self, plug_types=None):
     """Initialize plugs.
@@ -162,18 +153,18 @@ class TestExecutor(threads.KillableThread):
       return False
     except Exception:  # pylint: disable=broad-except
       # Record the equivalent failure outcome and exit early.
-      self._latest_outcome = phase_executor.PhaseExecutionOutcome(
+      self._last_outcome = phase_executor.PhaseExecutionOutcome(
           phase_executor.ExceptionInfo(*sys.exc_info()))
       return True
 
-  def _execute_test_start(self, phase_exec):
+  def _execute_test_start(self):
     """Run the start trigger phase, and check that the DUT ID is set after.
 
     Initializes any plugs used in the trigger.
     Logs a warning if the start trigger failed to set the DUT ID.
 
-    Args:
-      phase_exec: openhtf.core.phase_executor.PhaseExecutor instance.
+    The test start is special because we wait to initialize all other plugs
+    until this phase runs.
 
     Returns:
       True if there was a terminal error either setting up or running the test
@@ -185,51 +176,124 @@ class TestExecutor(threads.KillableThread):
         phase_plug.cls for phase_plug in self._test_start.plugs]):
       return True
 
-    self._latest_outcome = phase_exec.execute_phase(self._test_start)
-    if self._latest_outcome.is_terminal:
+    outcome = self._phase_exec.execute_phase(self._test_start)
+    if outcome.is_terminal:
+      self._last_outcome = outcome
       return True
 
     if self.test_state.test_record.dut_id is None:
       _LOG.warning('Start trigger did not set a DUT ID.')
     return False
 
-  def _execute_test_teardown(self, phase_exec):
-    phase_exec.stop(timeout_s=conf.cancel_timeout_s)
-    phase_exec.reset_stop()
+  def _stop_phase_executor(self, force=False):
+    with self._lock:
+      phase_exec = self._phase_exec
+      if not phase_exec:
+        # The test executor has not started yet, so no stopping is required.
+        return
+    if not force and not self._teardown_phases_lock.acquire(False):
+      # If locked, teardown phases are running, so do not cancel those.
+      return
+    try:
+      phase_exec.stop(timeout_s=conf.cancel_timeout_s)
+      # Resetting so phase_exec can run teardown phases.
+      phase_exec.reset_stop()
+    finally:
+      if not force:
+        self._teardown_phases_lock.release()
 
-    if self._do_teardown_function and self._test_options.teardown_function:
-      try:
-        timeout_s = self._test_options.teardown_function.options.timeout_s
-      except AttributeError:
-        # Force teardown function timeout, otherwise we can hang for a long time
-        # when shutting down, such as in response to a SIGINT.
-        timeout_s = conf.teardown_timeout_s
-
-      teardown_function = openhtf.PhaseDescriptor.wrap_or_copy(
-          self._test_options.teardown_function, timeout_s=timeout_s)
-
-      outcome = phase_exec.execute_phase(teardown_function)
-      # Ignore teardown phase outcome if there is already a terminal error.
-      if not self._latest_outcome or not self._latest_outcome.is_terminal:
-        self._latest_outcome = outcome
+  # TODO(kschiller): Cleanup the naming here and possibly merge with finalize.
+  def _execute_test_teardown(self):
     # Plug teardown does not affect the test outcome.
     self.test_state.plug_manager.tear_down_plugs()
 
     # Now finalize the test state.
-    if self._latest_outcome and self._latest_outcome.is_terminal:
-      self.test_state.finalize_from_phase_outcome(self._latest_outcome)
+    if self._abort.is_set():
+      self.test_state.logger.debug('Finishing test with outcome ABORTED.')
+      self.test_state.abort()
+    elif self._last_outcome and self._last_outcome.is_terminal:
+      self.test_state.finalize_from_phase_outcome(self._last_outcome)
     else:
       self.test_state.finalize_normally()
 
-  def _execute_test_phases(self, phase_exec):
-    """Executes one test's phases from start to finish."""
-    self.test_state.set_status_running()
+  def _handle_phase(self, phase):
+    if isinstance(phase, phase_group.PhaseGroup):
+      return self._execute_phase_group(phase)
 
-    try:
-      for phase in self._test_descriptor.phases:
-        self._latest_outcome = phase_exec.execute_phase(phase)
-        if self._latest_outcome.is_terminal:
+    self.test_state.logger.debug('Handling phase %s', phase.name)
+    outcome = self._phase_exec.execute_phase(phase)
+    if outcome.is_terminal and not self._last_outcome:
+      self._last_outcome = outcome
+
+    return outcome.is_terminal
+
+  def _execute_abortable_phases(self, type_name, phases, group_name):
+    """Execute phases, returning immediately if any error or abort is triggered.
+
+    Args:
+      type_name: str, type of phases running, usually 'Setup' or 'Main'.
+      phases: iterable of phase_descriptor.Phase or phase_group.PhaseGroup
+          instances, the phases to execute.
+      group_name: str or None, name of the executing group.
+
+    Returns:
+      True if there is a terminal error or the test is aborted, False otherwise.
+    """
+    if group_name and phases:
+      self.test_state.logger.debug(
+          'Executing %s phases for %s', type_name, group_name)
+    for phase in phases:
+      if self._abort.is_set() or self._handle_phase(phase):
+        return True
+    return False
+
+  def _execute_teardown_phases(self, teardown_phases, group_name):
+    """Execute all the teardown phases, regardless of errors.
+
+    Args:
+      teardown_phases: iterable of phase_descriptor.Phase or
+          phase_group.PhaseGroup instances, the phases to execute.
+      group_name: str or None, name of the executing group.
+
+    Returns:
+      True if there is at least one terminal error, False otherwise.
+    """
+    if group_name and teardown_phases:
+      self.test_state.logger.debug('Executing teardown phases for %s',
+                                   group_name)
+    ret = False
+    with self._teardown_phases_lock:
+      for teardown_phase in teardown_phases:
+        if self._full_abort.is_set():
+          ret = True
           break
-    except KeyboardInterrupt:
-      self.test_state.logger.debug('KeyboardInterrupt caught, aborting test.')
-      raise
+        if self._handle_phase(teardown_phase):
+          ret = True
+    return ret
+
+  def _execute_phase_group(self, group):
+    """Executes the phases in a phase group.
+
+    This will run the phases in the phase group, ensuring if the setup
+    phases all run without error that the teardown phases will also run, no
+    matter the errors during the main phases.
+
+    This function is recursive.  Do not construct phase groups that contain
+    themselves.
+
+    Args:
+      group: phase_group.PhaseGroup, the phase group to execute.
+
+    Returns:
+      True if the phases are terminal; otherwise returns False.
+    """
+    if group.name:
+      self.test_state.logger.debug('Entering PhaseGroup %s', group.name)
+    if self._execute_abortable_phases(
+        'setup', group.setup, group.name):
+      return True
+    main_ret = self._execute_abortable_phases(
+        'main', group.main, group.name)
+    teardown_ret = self._execute_teardown_phases(
+        group.teardown, group.name)
+    return main_ret or teardown_ret
