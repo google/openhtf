ae5d5cd97c8387c1969aa0efe8c9d8ad8cca722c
David Bensoussan <minipada@gmail.com>
2019-01-16 16:56:00 +0100

    cleanup: remove json unneeded references

--- openhtf/output/callbacks/junit.py
+++ openhtf/output/callbacks/junit.py
@@ -1,22 +1,22 @@
 """Module for outputting test record to JUNIT-formatted files."""
 
 import base64
+import six
 from junit_xml import TestSuite, TestCase
 
 from openhtf.output import callbacks
 from openhtf.util import data
-import six
 
 
 class OutputToJUNIT(callbacks.OutputToFile):
-  """Return an output callback that writes JSON Test Records.
+  """Return an output callback that writes JUNIT Test Records.
   Example filename_patterns might be:
-    '/data/test_records/{dut_id}.{metadata[test_name]}.json', indent=4)) or
+    '/data/test_records/{dut_id}.{metadata[test_name]}.xml', indent=4)) or
     '/data/test_records/%(dut_id)s.%(start_time_millis)s'
   To use this output mechanism:
     test = openhtf.Test(PhaseOne, PhaseTwo)
-    test.add_output_callback(openhtf.output.callbacks.OutputToJson(
-        '/data/test_records/{dut_id}.{metadata[test_name]}.json'))
+    test.add_output_callback(openhtf.output.callbacks.OutputToJUNIT(
+        '/data/test_records/{dut_id}.{metadata[test_name]}.xml'))
 
   Args:
     filename_pattern: A format string specifying the filename to write to,
@@ -25,16 +25,11 @@ class OutputToJUNIT(callbacks.OutputToFile):
     inline_attachments: Whether attachments should be included inline in the
       output. Set to False if you expect to have large binary attachments. If
       True (the default), then attachments are base64 encoded to allow for
-      binary data that's not supported by JSON directly.
+      binary data that's not supported by JUNIT directly.
   """
 
-  def __init__(self, filename_pattern=None, inline_attachments=True, **kwargs):
+  def __init__(self, filename_pattern=None, **kwargs):
     super(OutputToJUNIT, self).__init__(filename_pattern)
-    self.inline_attachments = inline_attachments
-
-    # Conform strictly to the JSON spec by default.
-    kwargs.setdefault('allow_nan', False)
-    self.allow_nan = kwargs['allow_nan']
     self.test_cases = []
 
   def serialize_test_record(self, test_record):
@@ -55,7 +50,7 @@ class OutputToJUNIT(callbacks.OutputToFile):
                      dict_test_record["dut_id"] + "." +
                      dict_test_record["metadata"]["test_name"],
                      (phase["end_time_millis"] -
-                      phase["start_time_millis"]) * 1000,
+                      phase["start_time_millis"]) / 1000,
                      "\n".join(output),
                      ''))
       else:
@@ -64,20 +59,18 @@ class OutputToJUNIT(callbacks.OutputToFile):
                      dict_test_record["dut_id"] + "." +
                      dict_test_record["metadata"]["test_name"],
                      (phase["end_time_millis"] -
-                      phase["start_time_millis"]) * 1000,
+                      phase["start_time_millis"]) / 1000,
                      "\n".join(output),
                      ''))
 
-    return TestSuite.to_xml_string([TestSuite("Test", self.test_cases)])
+    return TestSuite.to_xml_string([TestSuite("test", self.test_cases)])
 
   def convert_to_dict(self, test_record):
-    as_dict = data.convert_to_base_types(test_record,
-                                         json_safe=(not self.allow_nan))
+    as_dict = data.convert_to_base_types(test_record)
 
-    if self.inline_attachments:
-      for phase, original_phase in zip(as_dict['phases'], test_record.phases):
-        for name, attachment in six.iteritems(phase['attachments']):
-          original_data = original_phase.attachments[name].data
-          attachment['data'] = base64.standard_b64encode(
-              original_data).decode('utf-8')
+    for phase, original_phase in zip(as_dict['phases'], test_record.phases):
+      for name, attachment in six.iteritems(phase['attachments']):
+        original_data = original_phase.attachments[name].data
+        attachment['data'] = base64.standard_b64encode(
+            original_data).decode('utf-8')
     return as_dict
