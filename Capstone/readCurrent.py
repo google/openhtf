bb908dce125f35e3466c814d495ca145d47fab4e
szhouger@asu.edu <45433388+NikkiZhou@users.noreply.github.com>
2020-10-13 12:00:02 -0700

    added comment and return value.

--- Capstone/readCurrent.py
+++ Capstone/readCurrent.py
@@ -2,6 +2,13 @@
 
 import pyvisa
 class DMM34461A:
+	"""
+		Connect to DMM 34461A then get current value and store in dcCurrent;
+
+		returen:
+			dcCurrent
+	"""
+
 	def read_current(self):
 		
 		rm = pyvisa.ResourceManager()
@@ -11,6 +18,7 @@ class DMM34461A:
 
 		values = SCPI_34661A.query_ascii_values(':MEASure:CURRent:DC?')
 		dcCurrent = values[0]
+		return dcCurrent
 		SCPI_34661A.close()
 		rm.close()
 
