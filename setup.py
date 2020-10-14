9c67e42fa9f91c7d4f62112a48cf412471400369
Arun Sharma <arsharma@google.com>
2020-10-14 13:35:26 -0700

    Drop py2.7 tests from travis config and setup.py.

--- setup.py
+++ setup.py
@@ -191,7 +191,7 @@ setup(
                               'output/web_gui/dist/js/*',
                               'output/web_gui/dist/img/*',
                               'output/web_gui/*.*']},
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
+    python_requires='>=3.6',
     cmdclass={
         'build_proto': BuildProtoCommand,
         'clean': CleanCommand,
