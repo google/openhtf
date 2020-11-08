934b3af0cf31fce2fd46f7606e686a9d4edb58fb
Retr0djmarky <marcus.perez33099@gmail.com>
2020-11-08 14:19:31 -0700

    Forgotten Change
    Whoops, forgot to save a change

--- Capstone/plugE8267D.py
+++ Capstone/plugE8267D.py
@@ -24,26 +24,26 @@ class plugE8267D:
         """
         self.instrument.close()
 
-    # TODO: Set frequency on sig gen
+    # Set frequency on sig gen
     def set_frequency(self, hertz):
         if(self.check_frequency(hertz)):
             self.write(':SOURce:FREQuency:CW ' + str(hertz))
 
 
-    # TODO: Set output power on sig gen
+    # Set output power on sig gen
     def set_power(self, decibels):
         if(self.check_power(decibels)):
             self.write(':SOURce:POWer:LEVel:IMMediate:AMPLitude ' + str(decibels))
             
 
-    # TODO: Check if frequency is within the bounds
+    # Check if frequency is within the bounds
     def check_frequency(self, hertz):
         if(hertz < (25*10**3) or hertz > (20*10**9)):
             return False
         else:
             return True
 
-    # TODO: Check if frequency is within the bounds
+    # Check if frequency is within the bounds
     def check_power(self, decibels):
         if(decibels < (-90) or decibels > (25)):
             return False
