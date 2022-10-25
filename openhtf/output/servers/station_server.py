cbefb5c89e4a18f525b91fa58189b91fed80f172
Laurenz <git@laure.nz>
2022-10-25 16:27:43 +0200

    Don't try to set Content-Type if no mimetype is available

--- openhtf/output/servers/station_server.py
+++ openhtf/output/servers/station_server.py
@@ -360,7 +360,8 @@ class AttachmentsHandler(BaseTestHandler):
       self.set_status(404)
       return
 
-    self.set_header('Content-Type', attachment.mimetype)
+    if attachment.mimetype:
+      self.set_header('Content-Type', attachment.mimetype)
     self.write(attachment.data)
 
 
