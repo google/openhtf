5f5d307e6471a45f82653bf4d6c4b1514e8f9995
pocoJourno <124222107+pocoJourno@users.noreply.github.com>
2023-11-20 16:17:45 +1300

    remove customer_serial_number and manufacturer_serial_number
    add halter_serial_number
    dut_id will be the primary unique identifier - this may be the contract manufacturer's number or the halter serial number depending on the situation.

--- openhtf/core/dut_id.py
+++ openhtf/core/dut_id.py
@@ -5,12 +5,11 @@ from dataclasses import dataclass
 
 @dataclass
 class DutIdentifier:
-    customer_serial_number: str
-    manufacturer_serial_number: Optional[str] = None
+    halter_serial_number: str
     mac_address: Optional[str] = None
     part_number: Optional[str] = None
     additional: Optional[dict] = None
 
     @property
     def test_id(self) -> str:
-        return self.manufacturer_serial_number or self.customer_serial_number
+        return self.halter_serial_number
