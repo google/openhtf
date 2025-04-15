506587903baf350ead694cf1fdfd2ead296f26a1
Jean-Pierre Louis Communal <jpl.communal@deep-mine.com>
2025-04-15 16:51:36 +0200

    Use proper import statement and
    Also add units to measurement

--- examples/Resistor_Tutorial/main_test.py
+++ examples/Resistor_Tutorial/main_test.py
@@ -1,39 +1,33 @@
-"""main_test.py"""
-
 import time
 
-from plugs import MultimeterPlug, PowerSupplyPlug
-
+import openhtf as htf
 from openhtf.output.callbacks import console_summary
-from openhtf import Test
-from openhtf import measures, Measurement
-from openhtf import plug
-from openhtf.util import configuration
+from openhtf.util import configuration, units
 
+from resistor_plugs import MultimeterPlug, PowerSupplyPlug
 
 
 CONF = configuration.CONF
-SIMULATE_MODE = CONF.declare("simulate",
-                             default_value=False,
-                             description="Set simulation mode")
-CONF.load(simulate=True)
-
-
-SimulatedMultimeterPlug = configuration.bind_init_args(
-    MultimeterPlug, SIMULATE_MODE
-    )
-SimulatedPowerSupplyPlug = configuration.bind_init_args(
-    PowerSupplyPlug, SIMULATE_MODE
-    )
-
-@measures(
-        Measurement("resistor_val")
-        .doc("Computed resistor value")
-        .in_range(5320, 5880)
+SIMULATE_MODE = CONF.declare(
+    "simulate", default_value=False, description="Set simulation mode"
 )
-@plug(dmm=SimulatedMultimeterPlug)
-@plug(supply=SimulatedPowerSupplyPlug)
-def resistor_test(test, dmm:MultimeterPlug, supply:PowerSupplyPlug) -> None:
+CONF.load(simulate=False)  # Change to True if running simulated hardware
+
+
+MultimeterPlug = configuration.bind_init_args(MultimeterPlug, SIMULATE_MODE)
+
+PowerSupplyPlug = configuration.bind_init_args(PowerSupplyPlug, SIMULATE_MODE)
+
+
+@htf.measures(
+    htf.Measurement("resistor_val")
+    .doc("Computed resistor value")
+    .in_range(5320, 5880)
+    .with_units(units.OHM)
+)
+@htf.plug(dmm=MultimeterPlug)
+@htf.plug(supply=PowerSupplyPlug)
+def resistor_test(test, dmm: MultimeterPlug, supply: PowerSupplyPlug) -> None:
     """Check if resistor value in acceptable.
 
     Args:
@@ -44,22 +38,22 @@ def resistor_test(test, dmm:MultimeterPlug, supply:PowerSupplyPlug) -> None:
     supply.connect()
     dmm.connect()
 
-    input_voltage = 4 #in V
+    input_voltage = 4  # in V
     supply.set_voltage(input_voltage)
 
     time.sleep(3)
     current = float(dmm.read_current()[1])
-    measured_r = round(input_voltage/current, 3)
+    measured_r = round(input_voltage / current, 3)
     test.measurements["resistor_val"] = measured_r
 
     print(f"R value is: {measured_r}")
 
+
 def main():
-    """Create and execute the test. 
-    """
-    test = Test(resistor_test)
+    test = htf.Test(resistor_test)
     test.add_output_callbacks(console_summary.ConsoleSummary())
     test.execute()
+
 
 if __name__ == "__main__":
     main()
