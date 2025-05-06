6f630facd5743ffa3651d2fd1e1a9d064e8127bc
Joshua Monteiro <joshua.monteiro@halter.co.nz>
2025-05-06 17:56:49 +1200

    attempt to setup unmanaged plugs on instantiation

--- openhtf/plugs/__init__.py
+++ openhtf/plugs/__init__.py
@@ -261,7 +261,11 @@ class PlugManager(object):
       plug_instance: plug_type
       if plug_type in self._unmanaged_plugs:
         plug_instance = self._unmanaged_plugs[plug_type]
-        continue
+        try:
+          plug_instance.setUp()
+        except Exception:  # pylint: disable=broad-except
+          self.tear_down_plugs()
+          raise
       else:
         # Create a logger for this plug. All plug loggers go under the 'plug'
         # sub-logger in the logger hierarchy.
