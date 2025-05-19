4ef285e722f8388d82017ff0ab21dbe22db5130a
Akash Verma <akashv@google.com>
2025-05-19 16:11:43 -0700

    Update some comments and improve code quality. Add gitignore entry for VSCode default venv

--- test/examples/test_hello_world.py
+++ test/examples/test_hello_world.py
@@ -8,35 +8,39 @@ from openhtf.plugs import user_input
 
 
 class TestHelloWorld(unittest.TestCase):
+
     def setUp(self):
         self.expected_output_file = None
-
-    @mock.patch.object(user_input.UserInput, 'prompt')
-    def test_main_execution(self, mock_user_prompt):
-        # Configure the mock UserInput.prompt method
-        mock_user_prompt.return_value = "test_dut"
-
-        # Define the expected output file name
-        self.expected_output_file = "./test_dut.hello_world.json"
-
-        # Call the main function
-        hello_world.main()
-
-        # Assert that the output file was created
-        self.assertTrue(os.path.exists(self.expected_output_file))
-
-        # Assert that the DUT ID in the output file is correct
-        with open(self.expected_output_file) as f:
-            output_data = json.load(f)
-            print(output_data)
-        self.assertEqual(output_data['dut_id'], "test_dut")
-        self.assertEqual(output_data['outcome'], "PASS") # Verify test outcome
-        self.assertEqual(output_data['phases'][1]['measurements']['hello_world_measurement']['measured_value'], 'Hello Again!')
 
     def tearDown(self):
         if self.expected_output_file and os.path.exists(self.expected_output_file):
             os.remove(self.expected_output_file)
 
+    @mock.patch.object(user_input.UserInput, user_input.UserInput.prompt.__name__)
+    def test_main_execution(self, mock_user_prompt):
+        # The test only has one prompt, for the DUT ID.
+        mock_user_prompt.return_value = "test_dut"
 
-if __name__ == '__main__':
+        # The test configured JSON output into the current working directory.
+        self.expected_output_file = "./test_dut.hello_world.json"
+
+        hello_world.main()
+
+        # Assert that the output file was created
+        self.assertTrue(os.path.exists(self.expected_output_file))
+
+        with open(self.expected_output_file) as f:
+            output_data = json.load(f)
+        self.assertEqual(output_data["dut_id"], "test_dut")
+        self.assertEqual(output_data["outcome"], "PASS")
+        self.assertGreaterEqual(len(output_data["phases"]), 2)
+        self.assertEqual(
+            output_data["phases"][1]["measurements"]["hello_world_measurement"][
+                "measured_value"
+            ],
+            "Hello Again!",
+        )
+
+
+if __name__ == "__main__":
     unittest.main()
